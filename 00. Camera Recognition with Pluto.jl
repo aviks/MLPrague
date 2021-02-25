33475bc3988c567cadab746f6d1935edcd395c47
Avik Sengupta <avik@sengupta.net>
2021-02-25 23:56:07 +0000

    Initial Commit

---
+++ 00. Camera Recognition with Pluto.jl
@@ -0,0 +1,284 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
+# ╔═╡ 503ec810-77c0-11eb-1486-4f2855601f5d
+begin
+	using PlutoUI
+	using Metalhead
+	using Colors
+end
+
+# ╔═╡ 61caa9f0-77c0-11eb-1cc3-fb3ce27be5c0
+
+
+# ╔═╡ c7cf9410-77c3-11eb-29a0-a31943b55d57
+vgg = VGG19()
+
+# ╔═╡ 0bb15070-77c3-11eb-1e82-73e937bb397d
+function camera_input(;max_size=150, default_url="https://i.imgur.com/SUmi94P.png")
+"""
+<span class="pl-image waiting-for-permission">
+<style>
+	
+	.pl-image.popped-out {
+		position: fixed;
+		top: 0;
+		right: 0;
+		z-index: 5;
+	}
+
+	.pl-image #video-container {
+		width: 250px;
+	}
+
+	.pl-image video {
+		border-radius: 1rem 1rem 0 0;
+	}
+	.pl-image.waiting-for-permission #video-container {
+		display: none;
+	}
+	.pl-image #prompt {
+		display: none;
+	}
+	.pl-image.waiting-for-permission #prompt {
+		width: 250px;
+		height: 200px;
+		display: grid;
+		place-items: center;
+		font-family: monospace;
+		font-weight: bold;
+		text-decoration: underline;
+		cursor: pointer;
+		border: 5px dashed rgba(0,0,0,.5);
+	}
+
+	.pl-image video {
+		display: block;
+	}
+	.pl-image .bar {
+		width: inherit;
+		display: flex;
+		z-index: 6;
+	}
+	.pl-image .bar#top {
+		position: absolute;
+		flex-direction: column;
+	}
+	
+	.pl-image .bar#bottom {
+		background: black;
+		border-radius: 0 0 1rem 1rem;
+	}
+	.pl-image .bar button {
+		flex: 0 0 auto;
+		background: rgba(255,255,255,.8);
+		border: none;
+		width: 2rem;
+		height: 2rem;
+		border-radius: 100%;
+		cursor: pointer;
+		z-index: 7;
+	}
+	.pl-image .bar button#shutter {
+		width: 3rem;
+		height: 3rem;
+		margin: -1.5rem auto .2rem auto;
+	}
+
+	.pl-image video.takepicture {
+		animation: pictureflash 200ms linear;
+	}
+
+	@keyframes pictureflash {
+		0% {
+			filter: grayscale(1.0) contrast(2.0);
+		}
+
+		100% {
+			filter: grayscale(0.0) contrast(1.0);
+		}
+	}
+</style>
+
+	<div id="video-container">
+		<div id="top" class="bar">
+			<button id="stop" title="Stop video">✖</button>
+			<button id="pop-out" title="Pop out/pop in">⏏</button>
+		</div>
+		<video playsinline autoplay></video>
+		<div id="bottom" class="bar">
+		<button id="shutter" title="Click to take a picture">📷</button>
+		</div>
+	</div>
+		
+	<div id="prompt">
+		<span>
+		Enable webcam
+		</span>
+	</div>
+
+<script>
+	// based on https://github.com/fonsp/printi-static (by the same author)
+
+	const span = currentScript.parentElement
+	const video = span.querySelector("video")
+	const popout = span.querySelector("button#pop-out")
+	const stop = span.querySelector("button#stop")
+	const shutter = span.querySelector("button#shutter")
+	const prompt = span.querySelector(".pl-image #prompt")
+
+	const maxsize = $(max_size)
+
+	const send_source = (source, src_width, src_height) => {
+		const scale = Math.min(1.0, maxsize / src_width, maxsize / src_height)
+
+		const width = Math.floor(src_width * scale)
+		const height = Math.floor(src_height * scale)
+
+		const canvas = html`<canvas width=\${width} height=\${height}>`
+		const ctx = canvas.getContext("2d")
+		ctx.drawImage(source, 0, 0, width, height)
+
+		span.value = {
+			width: width,
+			height: height,
+			data: ctx.getImageData(0, 0, width, height).data,
+		}
+		span.dispatchEvent(new CustomEvent("input"))
+	}
+	
+	const clear_camera = () => {
+		window.stream.getTracks().forEach(s => s.stop());
+		video.srcObject = null;
+
+		span.classList.add("waiting-for-permission");
+	}
+
+	prompt.onclick = () => {
+		navigator.mediaDevices.getUserMedia({
+			audio: false,
+			video: {
+				facingMode: "environment",
+			},
+		}).then(function(stream) {
+
+			stream.onend = console.log
+
+			window.stream = stream
+			video.srcObject = stream
+			window.cameraConnected = true
+			video.controls = false
+			video.play()
+			video.controls = false
+
+			span.classList.remove("waiting-for-permission");
+
+		}).catch(function(error) {
+			console.log(error)
+		});
+	}
+	stop.onclick = () => {
+		clear_camera()
+	}
+	popout.onclick = () => {
+		span.classList.toggle("popped-out")
+	}
+
+	shutter.onclick = () => {
+		const cl = video.classList
+		cl.remove("takepicture")
+		void video.offsetHeight
+		cl.add("takepicture")
+		video.play()
+		video.controls = false
+		console.log(video)
+		send_source(video, video.videoWidth, video.videoHeight)
+	}
+	
+	
+	document.addEventListener("visibilitychange", () => {
+		if (document.visibilityState != "visible") {
+			clear_camera()
+		}
+	})
+
+
+	// Set a default image
+
+	const img = html`<img crossOrigin="anonymous">`
+
+	img.onload = () => {
+	console.log("helloo")
+		send_source(img, img.width, img.height)
+	}
+	img.src = "$(default_url)"
+	console.log(img)
+</script>
+</span>
+""" |> HTML
+end
+
+# ╔═╡ 10917120-77c2-11eb-1f2f-f799cdff4386
+@bind webcam_data camera_input()
+
+# ╔═╡ 2d943db0-77c3-11eb-30ed-77936fb4d210
+function process_raw_camera_data(raw_camera_data)
+	# the raw image data is a long byte array, we need to transform it into something
+	# more "Julian" - something with more _structure_.
+	
+	# The encoding of the raw byte stream is:
+	# every 4 bytes is a single pixel
+	# every pixel has 4 values: Red, Green, Blue, Alpha
+	# (we ignore alpha for this notebook)
+	
+	# So to get the red values for each pixel, we take every 4th value, starting at 
+	# the 1st:
+	reds_flat = UInt8.(raw_camera_data["data"][1:4:end])
+	greens_flat = UInt8.(raw_camera_data["data"][2:4:end])
+	blues_flat = UInt8.(raw_camera_data["data"][3:4:end])
+	
+	# but these are still 1-dimensional arrays, nicknamed 'flat' arrays
+	# We will 'reshape' this into 2D arrays:
+	
+	width = raw_camera_data["width"]
+	height = raw_camera_data["height"]
+	
+	# shuffle and flip to get it in the right shape
+	reds = reshape(reds_flat, (width, height))' / 255.0
+	greens = reshape(greens_flat, (width, height))' / 255.0
+	blues = reshape(blues_flat, (width, height))' / 255.0
+	
+	# we have our 2D array for each color
+	# Let's create a single 2D array, where each value contains the R, G and B value of 
+	# that pixel
+	
+	RGB.(reds, greens, blues)
+end
+
+# ╔═╡ fefc2b50-77bf-11eb-279d-6150cf6b17a6
+image = process_raw_camera_data(webcam_data)
+
+# ╔═╡ da80e31e-77c3-11eb-1578-6db7e58ad2c4
+classify(vgg, image)
+
+# ╔═╡ Cell order:
+# ╠═503ec810-77c0-11eb-1486-4f2855601f5d
+# ╠═10917120-77c2-11eb-1f2f-f799cdff4386
+# ╟─61caa9f0-77c0-11eb-1cc3-fb3ce27be5c0
+# ╠═fefc2b50-77bf-11eb-279d-6150cf6b17a6
+# ╠═c7cf9410-77c3-11eb-29a0-a31943b55d57
+# ╠═da80e31e-77c3-11eb-1578-6db7e58ad2c4
+# ╟─0bb15070-77c3-11eb-1e82-73e937bb397d
+# ╟─2d943db0-77c3-11eb-30ed-77936fb4d210
